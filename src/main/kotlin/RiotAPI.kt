decbc4d8e949b7b173d753d7b19dfe94d9921844
guney <guneyyilmaz2707@gmail.com>
2024-06-27 20:23:54 +0300

    feat: Account system

--- src/main/kotlin/RiotAPI.kt
+++ src/main/kotlin/RiotAPI.kt
@@ -9,24 +9,34 @@ import net.guneyilmaz0.riotapi.objects.match.Match
 
 @Suppress("unused")
 class RiotAPI(private val apiKey: String, private val region: Region, private val server: Server) {
-    fun getAccountByNameAndTag(name: String, tag: String) : Account {
-        val url = "https://${region.code}.api.riotgames.com/riot/account/v1/accounts/by-riot-id/${name.replace(" ", "%20")}/$tag?api_key=$apiKey"
+
+    fun getAccountByNameAndTag(string: String): Account =
+        getAccountByNameAndTag(string.split("#")[0], string.split("#")[1])
+
+    fun getAccountByNameAndTag(name: String, tag: String): Account {
+        val url = "https://${region.code}.api.riotgames.com/riot/account/v1/accounts/by-riot-id/${
+            name.replace(
+                " ",
+                "%20"
+            )
+        }/$tag?api_key=$apiKey"
         val json = JsonParser.parseString(Utils.getJsonFromUrl(url))
         return Gson().fromJson(json, Account::class.java)
     }
 
-    fun getMatchIdsByAccount(account: Account) : List<String> {
-        val url = "https://${region.code}.api.riotgames.com/lol/match/v5/matches/by-puuid/${account.puuid}/ids?start=0&count=20&api_key=$apiKey"
+    fun getMatchIdsByAccount(account: Account): List<String> {
+        val url =
+            "https://${region.code}.api.riotgames.com/lol/match/v5/matches/by-puuid/${account.puuid}/ids?start=0&count=20&api_key=$apiKey"
         val json = JsonParser.parseString(Utils.getJsonFromUrl(url))
         return json.asJsonArray.map { it.asString }
     }
 
-    fun getMatchById(id: String) : Match {
+    fun getMatchById(id: String): Match {
         val url = "https://${region.code}.api.riotgames.com/lol/match/v5/matches/$id?api_key=$apiKey"
         val json = JsonParser.parseString(Utils.getJsonFromUrl(url))
         return Gson().fromJson(json, Match::class.java)
     }
 
-    fun getLatestMatchByAccount(account: Account) : Match = getMatchById(getMatchIdsByAccount(account)[0])
+    fun getLatestMatchByAccount(account: Account): Match = getMatchById(getMatchIdsByAccount(account)[0])
 
 }
